44488c837f7d34f54a5d3ad634caad7588677f73
Xdiad47 <mail2diadem.com>
2021-01-16 20:11:19 +0530

    Final Commit

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,12 @@
 import React, {Fragment, useState} from 'react';
-import {Link} from 'react-router-dom'
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
+import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+import PropTypes from 'prop-types'
 
-export const Register = () => {
+
+export const Register = ({setAlert, register, isAuthenticated}) => {
     const[formData, setFormData] = useState({
         name: '',
         email: '',
@@ -15,11 +20,16 @@ export const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
-            console.log('Passwords do not match')
+           setAlert('Passwords do not match','danger')
         } else {
-          console.log('Success')
+          register({name, email, password});
         } 
+    };
+
+    if(isAuthenticated){
+      return <Redirect to='/dashboard'/>;
     }
+
     return (
         <Fragment>
 
@@ -27,11 +37,11 @@ export const Register = () => {
       <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
       <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)}  required />
+          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} />
         </div>
         <div className="form-group">
           <input type="email" placeholder="Email Address" name="email"
-          value={email} onChange={e => onChange(e)} required/>
+          value={email} onChange={e => onChange(e)} />
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -43,7 +53,7 @@ export const Register = () => {
             placeholder="Password"
             name="password"
             value={password} onChange={e => onChange(e)} 
-            minLength="6"
+            
           />
         </div>
         <div className="form-group">
@@ -52,7 +62,7 @@ export const Register = () => {
             placeholder="Confirm Password"
             name="password2"
             value={password2} onChange={e => onChange(e)} 
-            minLength="6"
+            
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -64,5 +74,16 @@ export const Register = () => {
      </Fragment>
     )
 }
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
-export default Register;
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect
+(mapStateToProps,
+{ setAlert, register })(Register);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
+
 const request = require('request');
 const config = require('config');
 const { response } = require('express');
@@ -148,7 +150,9 @@ router.get('/user/:user_id', async (req, res) => {
 
 router.delete('/', auth, async (req, res) => {
   try {
-    //@todo - remove users posts
+    //Remove user posts
+    await Post.deleteMany({ user: req.user.id });
+
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     //Remove User
